45b0419bc3efbe75f879edecebe5286286e9d21f
Antek Baranski <abaranski@roblox.com>
2013-08-01 17:03:33 -0700

    solr with a small S

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 maintainer       "Roblox Inc."
 maintainer_email "abaranski@roblox.com"
 license          "Apache 2.0"
-description      "Installs/Configures Solr"
+description      "Installs/Configures solr"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.1.3"
 
