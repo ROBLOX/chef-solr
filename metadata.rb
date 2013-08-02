823bec1f471695e84aaadcbcb900e745bb854390
Antek Baranski <abaranski@roblox.com>
2013-08-01 17:01:14 -0700

    House keeping changes

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
-maintainer       "Jiva Technology, Ltd."
-maintainer_email "theo@jivatechnology.com"
+maintainer       "Roblox Inc."
+maintainer_email "abaranski@roblox.com"
 license          "Apache 2.0"
-description      "Installs/Configures solr"
+description      "Installs/Configures Solr"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.1.2"
+version          "0.1.3"
 
 depends          "jetty"
