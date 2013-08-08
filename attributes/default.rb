19e4f204e3ca5c8b2bf6a4bca366d9e775392dff
Antek Baranski <abaranski@roblox.com>
2013-08-08 15:36:29 -0700

    cleanup ruby syntax around strings

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,9 +1,9 @@
-include_attribute "jetty"
-include_attribute "java"
+include_attribute 'jetty'
+include_attribute 'java'
 
 expand!
 
-default['solr']['version'] = "4.4.0"
+default['solr']['version'] = '4.4.0'
 default['solr']['checksum'] = nil #sha265
 
 if node['solr']['version'].split('.')[0].to_i >= 4 && node['solr']['version'].split('.')[1].to_i >= 1
@@ -17,7 +17,7 @@ default['solr']['war'] = "solr-#{node['solr']['version']}.war"
 default['solr']['context_path']  = '/solr'
 default['solr']['config_dir'] = '/etc/solr'
 default['solr']['prefix_home'] = '/usr/share'
-default['solr']['data'] = "#{node["jetty"]["webapp_dir"]}/solr/data"
+default['solr']['data'] = "#{node['jetty']['webapp_dir']}/solr/data"
 
 default['solr']['custom_config'] = nil
 default['solr']['custom_lib'] = nil
